ac788504eb89593ad67a862ed0d6cce0dd2f13f0
Nathan Henry <nhenry@girdsystems.com>
2021-10-29 13:13:12 -0400

    Added Procedure declariation as well as fixed the tree structure of expressions

--- parser/parser.py
+++ parser/parser.py
@@ -2,15 +2,12 @@ import visualizeTree as vt
 
 src_file = open("source.txt",'r')
 
-lookahead="hhhh"
-
-
-
+lookahead="Uninitialized"
 
 ###############################################################################################
 ###############################################################################################
-##                   I believe that there should be some sort of "token iterator"
-##      This depends if a potential first match could fail halfway through matches
+##               Currently Tokens are just strings
+##                Change to a token type with more information
 ###############################################################################################
 ###############################################################################################
 
@@ -71,17 +68,21 @@ def match(token_text):
       matchStack.append(lookahead)
       lookahead=get_token()
       return True
+    elif token_text=="<id>" and lookahead[0].isalpha():
+      matchStack.append(lookahead)
+      lookahead=get_token()
+      return True
     print("No match for", lookahead, token_text)
     return False
 
 def get_token():
-  global lookahead
-  print("Current Token: "+lookahead)
-  return src_file.readline().strip()
+  next_token = src_file.readline().strip() 
+  print("Working on: "+next_token)
+  return next_token
 
 def program_header(parent_node):
   new_node = Node("program_header")
-  if match("program") and match("test_program") and match("is"):
+  if match("program") and id(new_node) and match("is"):
     parent_node.add(new_node)
     return True
   else:
@@ -90,25 +91,83 @@ def program_header(parent_node):
 
 def variable_declaration(parent_node):
   new_node = Node("Variable_Declaration")
-  if match("variable") and (match("a") or match("b")) and match(":") and (match("integer") or match("bool")):
-    temp_type = matchStack.pop()
-    matchStack.pop() # un-needed colon
-    tempname=matchStack.pop()
-    matchStack.pop() # just to clear the stack
-    new_node.add(Node(tempname))
-    new_node.add(Node(temp_type))
+  if match("variable") and id(new_node) and match(":") and type_mark(new_node):
     parent_node.add(new_node)
     return True
   return False
-  
+
+def type_mark(parent_node):
+  new_node = Node("type_mark")
+  if match("integer") or match("float") or match("string") or match("bool"):
+    new_node.add(Node(matchStack.pop()))
+    parent_node.add(new_node)
+    return True
+  return False
+
+def parameter_list(parent_node):
+  global lookahead
+  new_node = Node("parameter_list")
+
+  if lookahead=="variable":
+    parameter(new_node)
+    match(";")
+    parameter_list(new_node)
+    match(";")
+    parent_node.add(new_node)
+    return True
+  return False
+
+def parameter(parent_node):
+  new_node = Node("parameter")
+  if variable_declaration(new_node):
+    return True
+  return False
+
+def procedure_header(parent_node):
+  new_node = Node("procedure_header")
+  temp_node = Node("")
+  if match("procedure") and id(temp_node) and match(":") and type_mark(new_node) and match("("):
+    matchStack.pop()
+    matchStack.pop()
+    matchStack.pop()
+    function_name = matchStack.pop()
+    parameter_list(new_node)
+    if match(")"):
+      parent_node.add(new_node)
+      return True
+    print("Fatal Error: Incomplete without the ')'")
+    exit()
+  return False
+
+def procedure_body(parent_node):
+  new_node = Node("procedure_body")
+  declaration_list(new_node)
+  match(";")
+
+  match("begin")
+
+  statement_list(new_node)
+  match(";")
+
+  return_statement(new_node)
+  match(";")
+    
+  if  match("end") and match("procedure"):
+    parent_node.add(new_node)
+    return True
+    
 def procedure_declaration(parent_node):
+  new_node = Node("procedure_delcaration")
+  if procedure_header(new_node) and procedure_body(new_node):
+    parent_node.add(new_node)
+    return True
   return False
 
 def declaration_list(parent_node):
   global lookahead
   new_node = Node("declaration_list")
 
-  if lookahead=="variable":
+  if lookahead=="variable" or lookahead=="global" or lookahead=="procedure":
     declaration(new_node)
     match(";")
     declaration_list(new_node)
@@ -142,27 +201,120 @@ def declaration(parent_node):
 #5 + 2 - 1 + 9
 def arith_op(parent_node):
   new_node = Node("arith_op")
+  print("arith op right now")
   if relation(new_node) and arith_op_prime(new_node):
     parent_node.add(new_node)
     return True
   return False
 
 def arith_op_prime(parent_node):
-  new_node = Node("Factor")
-  if (match("+") or match("-")) and relation(new_node) and arith_op_prime(new_node):
-    parent_node.name = matchStack.pop()
+  new_node = Node("arith_value")
+  if (match("+") or match("-")):
+    operation_name = matchStack.pop()
+    if relation(new_node) and arith_op_prime(new_node):
+      parent_node.name = operation_name
+      parent_node.add(new_node)
+      return True
+  return True
+
+
+####  Left Recursion Elimination Notes  ####
+# term = term (* or /) Factor
+# term = Factor
+
+# term = match(factor) and term_prime
+# term_prime = (match("*") or match("/")) and factor() and term_prime
+# term_prime = empty
+def term(parent_node):
+  new_node = Node("term")
+  if factor(new_node) and term_prime(new_node):
+    print("In here")
     parent_node.add(new_node)
     return True
-  else:
-    return True
+  return False
 
-def relation(parent_node):
-  new_node = Node("")
-  if match("<number>"):
-    new_node.name=matchStack.pop()
+def term_prime(parent_node):
+  new_node = Node("term_prime")
+  if (match("*") or match("/")) and factor(new_node) and term_prime(new_node):
+    parent_node.name =- matchStack.pop()
     parent_node.add(new_node)
     return True
   return True
+
+
+
+
+def relation(parent_node):
+  new_node = Node("relation")
+  if term(new_node):
+    #new_node.name=matchStack.pop()
+    parent_node.add(new_node)
+    return True
+  return False
+
+def factor(parent_node):
+  new_node = Node("factor")
+
+  if match("(") and expression(new_node) and match(")"):
+    parent_node.add(new_node)
+    return True
+  if procedure_call(new_node):
+    parent_node.add(new_node)
+    return True
+  if string(new_node):
+    parent_node.add(new_node)
+    return True
+  if match("true"):
+    new_node.name = "True"
+    parent_node.add(new_node)
+    return True
+  if match("false"):
+    new_node.name = "False"
+    parent_node.add(new_node)
+    return True
+
+  # Optional Minus sign
+  match("-")
+  if name(new_node):
+    parent_node.add(new_node)
+    return True
+  if number(new_node):
+    parent_node.add(new_node)
+    return True
+  
+  return False
+
+
+def string(parent_node):
+  new_node = Node("")
+  if match("<string>"):
+    new_node.name = matchStack.pop()
+    parent_node.add(new_node)
+    return True
+  return False
+
+def number(parent_node):
+  new_node = Node("")
+  if match("<number>"):
+    new_node.name = matchStack.pop()
+    parent_node.add(new_node)
+    return True
+  return False
+
+def name(parent_node):
+  new_node = Node("name")
+  if id(new_node):
+    if match("[") and expression(new_node) and match("]"):
+      matchStack.pop()
+      new_node.name = new_node.name + matchStack.pop()
+    parent_node.add(new_node)
+    return True
+  return False
+  
+
+def procedure_call(parent_node):
+  print("Update Procedure Call function")
+  return False
 
 
 # Expression is another example of needing a "prime"
@@ -177,6 +329,7 @@ def expression(parent_node):
   return False
 
 def expression_prime(parent_node):
+  print("HERE RIGHT NOW")
   new_node = Node("")
   if match("&") and expression(new_node):
     new_node.name = "& (and)"
@@ -193,9 +346,7 @@ def assignment_statement(parent_node):
   new_node = Node("assignment_statement")
   #if (match("a") or match("b")) and match(":=") and match("<number>"):
   print("Trying out expression, this might cause errors")
-  if (match("a") or match("b")) and match(":="):
-    matchStack.pop()
-    new_node.add(Node(matchStack.pop()+" ="))
+  if id(new_node) and match(":="):
     if expression(new_node):
       parent_node.add(new_node)
       return True
@@ -223,9 +374,13 @@ def loop_statement(parent_node):
   return False
 
 def return_statement(parent_node):
+  print("In Fucntion Return_statatemtn")
   new_node = Node("return_statement")
   if match("return") and expression(new_node):
-    parent_node.add(new_node)
+    parent_node.name = "Return Statement Found"
+    print("The two branch appoach is starting to be an issue.")
+    print("Either way it seems like the best approach might be to either a seperate tree for each function")
+    print("Or create a very long invisible link between the two. Invsiible links are a real thing in graphviz")
     return True
   return False
 
@@ -241,10 +396,11 @@ def statement_list(parent_node):
   global lookahead
   new_node = Node("statement_list")
 
-  if lookahead=="a" or lookahead=="b" or lookahead=="for" or lookahead=="return" or lookahead=="if":
+  if lookahead=="a" or lookahead=="b" or lookahead=="y" or lookahead=="x" or lookahead=="for" or lookahead=="if":
     statement(new_node)
     match(";")
     statement_list(new_node)
+    match(";")
     parent_node.add(new_node)
     return True
   return False
@@ -252,7 +408,6 @@ def statement_list(parent_node):
 
 def program_body(parent_node):
   new_node = Node("program_body")
-  global lookahead
 
   declaration_list(new_node)
   match(";")
@@ -268,10 +423,18 @@ def program_body(parent_node):
   return False
 
 
+def id(parent_node):
+  new_node = Node("id")
+  if match("<id>"):
+    new_node.name = new_node.name + " : "+matchStack.pop()
+    parent_node.add(new_node)
+    return True
+  return False
+
+
 
 def program(parent_node):
   new_node = Node("program")
-  global lookahead
   if program_header(new_node) and program_body(new_node):
     parent_node.add(new_node)
     return True
@@ -284,6 +447,8 @@ def main():
   lookahead = get_token()
 
   start_node = Node("root")
+
+  # Kicks off the entire parseing
   if program(start_node):
     print("Success!!")
 
