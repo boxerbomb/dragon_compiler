d158e313f8769eb1bd365d61d941a56aa210c5d7
nathan henry <boxerbomb@debian-BULLSEYE-live-builder-AMD64>
2021-12-19 14:19:06 -0500

    dump

--- parser/parser.py
+++ parser/parser.py
@@ -553,6 +553,27 @@ def runUnitTest(filename):
     print("Error in program")
     return False
 
+
+
+def solve(root):
+      if not root:
+         return root
+
+      if len(root.children)==0:
+         # Return an end Node
+         return root
+
+      if len(root.children)==1:
+        return solve(root.children[0])
+
+      if len(root.children)>=2:
+        root.children[0] = solve(root.children[0])
+        root.children[1] = solve(root.children[1])
+
+      return root
+
+
+
 def main():
   lexer.openFile("../src_files/test1b.src")
   global lookahead
@@ -568,9 +589,9 @@ def main():
     #printPreorder(start_node)
 
     for function_root in root_nodes:
-      #print_preorder(root)
       viz = vt.ParseTreeVisualizer()
-      viz.gendot(function_root)
+      new_root = solve(function_root)
+      viz.gendot(new_root)
 
   else:
     print("Error in program")
